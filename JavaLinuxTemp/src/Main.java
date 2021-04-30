9d00904ca28620abe7d0e60c36964c8e4bf477a3
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2021-04-30 12:26:51 -0400

    p

--- JavaLinuxTemp/src/Main.java
+++ JavaLinuxTemp/src/Main.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) {
+		//temp
 		ProcessBuilder processBuilder = new ProcessBuilder();
 		processBuilder.command("bash", "-c", "sh /root/Desktop/scriptA.sh");
 		try {
