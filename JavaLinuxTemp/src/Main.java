c93da07e23e89df7c5a32579dfdc7702996e8804
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2021-04-30 11:55:55 -0400

    p

--- JavaLinuxTemp/src/Main.java
+++ JavaLinuxTemp/src/Main.java
@@ -1,8 +1,31 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World; Hello");
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		processBuilder.command("bash", "-c", "sh /root/Desktop/scriptA.sh");
+		try {
+			Process process = processBuilder.start();
+			StringBuilder output = new StringBuilder();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			String line;
+			while(	(line=reader.readLine()) != null	) {
+				output.append(line+"\n");
+			}
+			int exitVal = process.waitFor();
+			if(exitVal == 0) {
+				System.out.println("SUCCESS");
+			} else {
+				System.out.println("FAILED");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
