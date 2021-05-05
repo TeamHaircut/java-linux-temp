377d77e623d50a5bd58f60f4898302229955b99e
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2021-05-05 11:18:19 -0400

    p

--- JavaLinuxTemp/src/teamhaircut/Main.java
+++ JavaLinuxTemp/src/teamhaircut/Main.java
@@ -163,7 +163,8 @@ public class Main extends JPanel implements ActionListener, PropertyChangeListen
             }
         });
  
-        Path dir = Paths.get("C:/Users/RuthDan/Desktop/test");
+        //Path dir = Paths.get("C:/Users/RuthDan/Desktop/test");
+        Path dir = Paths.get("/root/Desktop/test");
         boolean recursive = false;
         try {
 			new WatchDir(dir, recursive).processEvents();
