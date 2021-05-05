1485ae41f5554f7052c05b6d57c367baebebafe6
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2021-05-05 14:08:03 -0400

    p

--- JavaLinuxTemp/src/teamhaircut/Main.java
+++ JavaLinuxTemp/src/teamhaircut/Main.java
@@ -145,7 +145,8 @@ public class Main extends JPanel implements ActionListener, PropertyChangeListen
         //we create new instances as needed.
         /////////////////////////////////////////////////////////////////////////////////////////////////
 		ProcessBuilder processBuilder = new ProcessBuilder();
-		processBuilder.command("/bin/bash", "-c", "sh /root/Desktop/createFile.sh");
+		//processBuilder.command("/bin/bash", "-c", "sh /root/Desktop/createFile.sh");
+		processBuilder.command("nohup", "sh", "/root/Desktop/createFile.sh");
 		try {
 			Process process = processBuilder.start();
 //			StringBuilder output = new StringBuilder();
