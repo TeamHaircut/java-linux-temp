699835432acc4ba0d3ac70f6c68cbc09863f2057
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2021-05-05 15:38:34 -0400

    p

--- JavaLinuxTemp/src/teamhaircut/Main.java
+++ JavaLinuxTemp/src/teamhaircut/Main.java
@@ -277,7 +277,7 @@ class WatchDir1 {
 /////////////////////////////////EDIT HERE/////////////////////////////////
                 try {
                 	Main.prog = (int)((float) ((Files.size(child)/864f)*100));
-                	if(child.toString().equals("/root/Desktop/test/helper.log") && event.kind().name().equals(ENTRY_MODIFY)) {
+                	if(child.toString().equals("/root/Desktop/test/helper.log") && event.kind().name().equals(ENTRY_MODIFY.toString())) {
                 		System.out.println("HERE");
                 		String msg = "";
                 		switch((int)Files.size(child)) {
